c427014d3777b20598016714beb13c0a869c398a
lucalLevi2511 <luclevi0102@gmail.com>
2024-10-15 21:54:25 -0300

    nome dev

--- js/index.js
+++ js/index.js
@@ -1,3 +1,58 @@
 btn.onclick= function(){
-    console.log("Dia ${dia.value}, Mes ${mes.value}")
+    if(Number(dia.value) % 10 == 1){
+        console.log("");
+    } else if(Number(dia.value) % 10 == 2){
+        console.log("");
+    } else if(Number(dia.value) % 10 == 3) {
+        console.log("");
+    } else if(Number(dia.value) % 10 == 4) {
+        console.log("");
+    } else if(Number(dia.value) % 10 == 5) {
+        console.log("");
+    } else if(Number(dia.value) % 10 == 6) {
+        console.log("");
+    }
+    
+    switch(Number(mes.value)){
+        case 1:
+            console.log("bugado(a);");
+        break;
+        case 2:
+            console.log("do CTRL C, CTRL V;");
+        break;
+        case 3:
+            console.log("das gambiarras;");
+        break;
+        case 4:
+            console.log("que culpa o cache;");
+        break;
+        case 5:
+            console.log("que esquece o que faz;");
+        break;
+        case 6:
+            console.log("do git vazio;");
+        break;
+        case 7:
+            console.log("das try/catch vazia;");
+        break;
+        case 8:
+            console.log("famosinho do linkedin;");
+        break;
+        case 9:
+            console.log("caçador de bugs;");
+        break;
+        case 10:
+            console.log("do windows pirata;");
+        break;
+        case 11:
+            console.log("do update sem where;");
+        break;
+        case 12:
+            console.log("do commit bugado;");
+        break;
+    }
+    
 }
+        
+    
+
