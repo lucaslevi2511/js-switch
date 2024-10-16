cdc373ad51299129ec720a2e0e04da815d1ebee8
Lucas Levi Santos de Medeiros <lucas0860@digitalcollegesul.local>
2024-10-16 08:57:13 -0300

    ainda tem erros no nome dev

--- js/index.js
+++ js/index.js
@@ -1,57 +1,53 @@
 btn.onclick= function(){
-    if(Number(dia.value) % 10 == 1){
-        console.log("");
-    } else if(Number(dia.value) % 10 == 2){
-        console.log("");
-    } else if(Number(dia.value) % 10 == 3) {
-        console.log("");
-    } else if(Number(dia.value) % 10 == 4) {
-        console.log("");
-    } else if(Number(dia.value) % 10 == 5) {
-        console.log("");
-    } else if(Number(dia.value) % 10 == 6) {
-        console.log("");
+    let digito = Number(dia.value.substr(-1));
+    if(digito > 5 && digito < 9 ){
+        nome.innerHTML = ("Estágiaro(a)");
+    } else if(digito > 0 && digito < 3){
+        nome.innerHTML = ("Desenvolvedor(a)");
+    } else if(digito > 2 && digito < 6) {
+        nome.innerHTML = ("Programador(a)");
+    } else if (digito == 0 && digito == 9) {
+        nome.innerHTML = ("Sênior(a)");
     }
     
     switch(Number(mes.value)){
         case 1:
-            console.log("bugado(a);");
+            nome.innerHTML += ("bugado(a);");
         break;
         case 2:
-            console.log("do CTRL C, CTRL V;");
+            nome.innerHTML += ("do CTRL C, CTRL V;");
         break;
         case 3:
-            console.log("das gambiarras;");
+            nome.innerHTML += ("das gambiarras;");
         break;
         case 4:
-            console.log("que culpa o cache;");
+            nome.innerHTML += ("que culpa o cache;");
         break;
         case 5:
-            console.log("que esquece o que faz;");
+            nome.innerHTML += "que esquece o que faz;";
         break;
         case 6:
-            console.log("do git vazio;");
+            nome.innerHTML += "do git vazio;";
         break;
         case 7:
-            console.log("das try/catch vazia;");
+            nome.innerHTML += "das try/catch vazia;";
         break;
         case 8:
-            console.log("famosinho do linkedin;");
+            nome.innerHTML += "famosinho do linkedin;";
         break;
         case 9:
-            console.log("caçador de bugs;");
+            nome.innerHTML += "caçador de bugs;";
         break;
         case 10:
-            console.log("do windows pirata;");
+            nome.innerHTML += "do windows pirata;";
         break;
         case 11:
-            console.log("do update sem where;");
+            nome.innerHTML += "do update sem where;";
         break;
         case 12:
-            console.log("do commit bugado;");
+            nome.innerHTML += "do commit bugado;";
         break;
     }
-    
 }
         
     
